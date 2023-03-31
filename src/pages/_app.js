ab7a318997bbe505ffcaa6ae6a4ee452b5445973
abraham <abraham.turnsek@gmail.com>
2023-03-31 13:38:00 +0200

    tgm añadido

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,15 @@
 import "@/styles/globals.css";
-//import Layout from "@/componentes/Layout/Layout";
+import { useEffect } from "react";
+import TagManager from "react-gtm-module";
 
 export default function App({ Component, pageProps }) {
+  const gtmId = process.env.NEXT_PUBLIC_GTM || "";
+  const tagManagerArgs = {
+    gtmId,
+  };
+
+  useEffect(() => {
+    TagManager.initialize(tagManagerArgs);
+  }, []);
   return <Component {...pageProps} />;
 }
