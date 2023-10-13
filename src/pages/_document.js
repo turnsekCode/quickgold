ebc4aa8b84bc1f21b9ebf17cfb254f5dcf6a6624
abraham <abraham.turnsek@gmail.com>
2023-10-13 11:09:44 +0200

    quitado del script nuevo

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,16 +31,6 @@ export default function Document() {
           href="https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.css"
         />
 </noscript>*/}
-      <script>
-        {`(function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)}
-        h._hjSettings={hjid:2628215,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')`}
-      </script>
       <Head />
       <body>
         <Main />
